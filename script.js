5d3587322cbe6a0ff7377764050b158933c89a56
Eduardo Lulo <eduardo.lulo.m@gmail.com>
2025-11-22 14:27:53 +0000

    Updates

--- script.js
+++ script.js
@@ -115,3 +115,17 @@ function calculation(e) {
 }
 
 // ==========================================================
+
+function contarVocales(str) {
+  let resultado = 0;
+
+  str.split("").forEach((l) => {
+    if ("AEIOUaeiou".includes(l)) {
+      resultado++;
+    }
+  });
+
+  return resultado;
+}
+
+console.log(contarVocales("hola mundooereeeeeee"));
